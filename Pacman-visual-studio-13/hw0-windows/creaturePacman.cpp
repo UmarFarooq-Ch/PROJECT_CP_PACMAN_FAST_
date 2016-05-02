4ac9a3b9b3a08ff214effe0bec835543a1996759
Umar Farooq <umarch@live.com>
2016-05-02 15:13:10 +0500

    v1.0.2

--- Pacman-visual-studio-13/hw0-windows/creaturePacman.cpp
+++ Pacman-visual-studio-13/hw0-windows/creaturePacman.cpp
@@ -17,8 +17,8 @@ void creaturePacman::nextMove(void)
 	//now first i will check where is the direction of mouth of pacman
 	if (m_n_eyesDirection == LEFT_)
 	{
-		int nextMidValueOftempX = (tempX) * 20 - 10; cout << "Next MidValue : " << nextMidValueOftempX << endl;
-		int sameMidValueOftempY = (tempY) * 20 + 10; cout << "Next Mid Value of Y: " << sameMidValueOftempY << endl;
+		//int nextMidValueOftempX = (tempX) * 20 - 10; cout << "Next MidValue : " << nextMidValueOftempX << endl;
+		//int sameMidValueOftempY = (tempY) * 20 + 10; cout << "Next Mid Value of Y: " << sameMidValueOftempY << endl;
 		//now check which board part lies in tempX and tempY+1 /*yani same Y block*/
 		int nextPart = Board::getBoardPart(tempX - 1, 35 - tempY);
 		if (nextPart == 16 || nextPart == 17)	//if there is eatable or empty space then move the pacman
@@ -28,41 +28,32 @@ void creaturePacman::nextMove(void)
 		}
 		else	//else move pacman only two times so that pacman reach to the mid point of that tile
 		{
-			if (check >= 2)
+			if (check >= 1)
 				m_n_move = false;
-			++check;
-			if (check < 2)
+			if (check < 1)
 				m_n_xVertex -= 5;
+			++check;
 		}
 		Board::setBoardPart(tempX,35- tempY, 17); //set eatable to moveable empty space
 	}
 
 	else if (m_n_eyesDirection == RIGHT_)
 	{
-		tempX = this->m_n_xVertex + 2;
-		tempY = this->m_n_yVertex;
-		int nextItem = Board::getBoardPart(tempX / 20, (720 - tempY) / 20);	//checking which item lies in tempX,tempY position
-		if (nextItem == 16) //&& (Board::getBoardPart((tempX / 20) + 1, (720 - tempY) / 20) == 16 || ((tempX / 20) * 19 - 15) > m_n_xVertex))	//if it is a food then change the x vertex and y vertex accordingly and increase the score
+		int nextPart = Board::getBoardPart(tempX + 1, 35 - tempY);
+		if (nextPart == 16 || nextPart == 17)	//if there is eatable or empty space then move the pacman
 		{
 			m_n_xVertex += 5;
-			if (getBoardPart(this->m_n_xVertex / 20, (720 - this->m_n_yVertex) / 20) == 16)
-			{
-				//score += 10;
-				setBoardPart(this->m_n_xVertex / 20, ((720 - this->m_n_yVertex) / 20), 17);
-			}
-			//if (Board::getBoardPart((tempX / 20) + 1, (720 - tempY) / 20) != 16 && Board::getBoardPart((tempX / 20) + 1, (720 - tempY) / 20) != 17)
-			//	move = false;
+			check = 0;
 		}
+		else	//else move pacman only two times so that pacman reach to the mid point of that tile
+		{
+			if (check >= 2)
+				m_n_move = false;
+			if (check < 2)
+				m_n_xVertex += 5;
+			++check;
+		}
+		Board::setBoardPart(tempX, 35 - tempY, 17); //set eatable to moveable empty space
 
-		else if (nextItem == 17)
-		{
-			m_n_xVertex += 5;
-		}
-		else
-		{
-			m_n_move = false;
-			m_n_xVertex -= 10;
-			cout << "Else executed" << endl;
-		}
 	}
 }
