8adf0c83dcb62c2e25c74681d82b619293b7ee5f
Umar Farooq <umarch@live.com>
2016-05-02 15:15:48 +0500

    v1.0.2

--- Pacman-visual-studio-13/hw0-windows/creaturePacman.h
+++ Pacman-visual-studio-13/hw0-windows/creaturePacman.h
@@ -14,7 +14,7 @@ public:
 	/*nextAllowed*/
 	creaturePacman(const int & pX = 280, const int & pY = 190, const string & pNameOfCreature = "pacman", const ColorNames & pValue = YELLOW, const bool & pAliveStatus = true, const int & pEyesDirection = LEFT_, const float & pValueRadian = 6.0);
 	virtual ~creaturePacman();
-	void nextMove(void);
+	void nextMove(void);	//draw function call it so it this fuunction will change pacman vertexs according to its direction and position
 	inline void setMove(const bool & pCheck = false) { m_n_move = pCheck; }
 	inline bool getMove(void) const { return this->m_n_move; }
 	inline float getRadian(void) const { return this->m_n_radians; }
